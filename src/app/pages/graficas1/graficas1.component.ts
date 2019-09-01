382a2830883092a7df317a5689a141727227853b
Richard Fuentevilla <rfuentevillar@gmail.com>
2019-09-01 00:22:58 -0400

    Modificaciones en los modulos

--- src/app/pages/graficas1/graficas1.component.ts
+++ src/app/pages/graficas1/graficas1.component.ts
@@ -3,13 +3,76 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-graficas1',
   templateUrl: './graficas1.component.html',
-  styles: []
+  styles: [`
+      * {
+        font-size: 12px;
+        font-family: 'Times New Roman';
+      }
+      td,
+      th,
+      tr,
+      table {
+        border-top: 1px solid black;
+        border-collapse: collapse;
+      }
+      td.producto,
+      th.producto {
+        width: 75px;
+        max-width: 75px;
+      }
+      td.cantidad,
+      th.cantidad {
+        width: 40px;
+        max-width: 40px;
+        word-break: break-all;
+      }
+      td.precio,
+      th.precio {
+        width: 40px;
+        max-width: 40px;
+        word-break: break-all;
+      }
+      .centrado {
+        text-align: center;
+        align-content: center;
+      }
+      .ticket {
+        width: 155px;
+        max-width: 155px;
+      }
+      img {
+        max-width: 100px;
+        width: 100px;
+      }
+      @media print{
+        .oculto-impresion, .oculto-impresion *{
+          display: none !important;
+        }
+      }
+  `]
 })
 export class Graficas1Component implements OnInit {
 
   constructor() { }
 
+  visible = true;
+
   ngOnInit() {
+  }
+
+  imprimerTicket() {
+    this.visible = false;
+
+    // ticket
+    const printContents = document.getElementById('ticket').innerHTML;
+    const originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+
   }
 
 }
